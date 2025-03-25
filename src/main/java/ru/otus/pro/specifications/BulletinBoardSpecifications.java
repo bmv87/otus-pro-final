e4a785011bb7bbc8aa58761866efbbfc70039179
BMV <venerasmith@mail.ru>
2025-03-26 01:33:04 +0300

    fix: dateTime specifications for filters

--- src/main/java/ru/otus/pro/specifications/BulletinBoardSpecifications.java
+++ src/main/java/ru/otus/pro/specifications/BulletinBoardSpecifications.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.domain.Specification;
 import ru.otus.pro.entities.BulletinBoard;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class BulletinBoardSpecifications {
 
@@ -16,27 +17,26 @@ public class BulletinBoardSpecifications {
     }
 
     public static Specification<BulletinBoard> hasName(String value) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get(BulletinBoard.Fields.name), "%" + value + "%");
+        return (root, query, criteriaBuilder) -> criteriaBuilder.like(criteriaBuilder.lower(root.get(BulletinBoard.Fields.name)), "%" + value.toLowerCase() + "%");
     }
 
     public static Specification<BulletinBoard> hasDescription(String value) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get(BulletinBoard.Fields.description), "%" + value + "%");
+        return (root, query, criteriaBuilder) -> criteriaBuilder.like(criteriaBuilder.lower(root.get(BulletinBoard.Fields.description)), "%" + value.toLowerCase() + "%");
     }
 
-
     public static Specification<BulletinBoard> createDateBetween(LocalDateTime startDate, LocalDateTime endDate) {
-        var start = startDate.with(LocalDateTime.MIN);
-        var end = endDate.with(LocalDateTime.MAX);
+        var start = startDate.toLocalDate().atStartOfDay();
+        var end = endDate.toLocalDate().atTime(LocalTime.MAX);
         return (root, query, criteriaBuilder) -> criteriaBuilder.between(root.get(BulletinBoard.Fields.createDate), start, end);
     }
 
     public static Specification<BulletinBoard> createDateLessThanOrEqualTo(LocalDateTime endDate) {
-        var end = endDate.with(LocalDateTime.MAX);
+        var end = endDate.toLocalDate().atTime(LocalTime.MAX);
         return (root, query, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(root.get(BulletinBoard.Fields.createDate), end);
     }
 
     public static Specification<BulletinBoard> createDateGreaterThanOrEqualTo(LocalDateTime startDate) {
-        var start = startDate.with(LocalDateTime.MIN);
+        var start = startDate.toLocalDate().atStartOfDay();
         return (root, query, criteriaBuilder) -> criteriaBuilder.greaterThanOrEqualTo(root.get(BulletinBoard.Fields.createDate), start);
     }
 }
--- src/main/java/ru/otus/pro/specifications/BulletinSpecifications.java
+++ src/main/java/ru/otus/pro/specifications/BulletinSpecifications.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.domain.Specification;
 import ru.otus.pro.entities.Bulletin;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class BulletinSpecifications {
 
@@ -12,30 +13,30 @@ public class BulletinSpecifications {
     }
 
     public static Specification<Bulletin> hasTitle(String value) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get(Bulletin.Fields.title), "%" + value + "%");
+        return (root, query, criteriaBuilder) -> criteriaBuilder.like(criteriaBuilder.lower(root.get(Bulletin.Fields.title)), "%" + value.toLowerCase() + "%");
     }
 
     public static Specification<Bulletin> hasContent(String value) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get(Bulletin.Fields.content), "%" + value + "%");
+        return (root, query, criteriaBuilder) -> criteriaBuilder.like(criteriaBuilder.lower(root.get(Bulletin.Fields.content)), "%" + value.toLowerCase() + "%");
     }
 
     public static Specification<Bulletin> createDateBetween(LocalDateTime startDate, LocalDateTime endDate) {
-        var start = startDate.with(LocalDateTime.MIN);
-        var end = endDate.with(LocalDateTime.MAX);
+        var start = startDate.toLocalDate().atStartOfDay();
+        var end = endDate.toLocalDate().atTime(LocalTime.MAX);
         return (root, query, criteriaBuilder) -> criteriaBuilder.between(root.get(Bulletin.Fields.createDate), start, end);
     }
 
     public static Specification<Bulletin> createDateLessThanOrEqualTo(LocalDateTime endDate) {
-        var end = endDate.with(LocalDateTime.MAX);
+        var end = endDate.toLocalDate().atTime(LocalTime.MAX);
         return (root, query, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(root.get(Bulletin.Fields.createDate), end);
     }
 
     public static Specification<Bulletin> createDateGreaterThanOrEqualTo(LocalDateTime startDate) {
-        var start = startDate.with(LocalDateTime.MIN);
+        var start = startDate.toLocalDate().atStartOfDay();
         return (root, query, criteriaBuilder) -> criteriaBuilder.greaterThanOrEqualTo(root.get(Bulletin.Fields.createDate), start);
     }
 
     public static Specification<Bulletin> isActive(LocalDateTime date) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.between(criteriaBuilder.literal(date),root.get(Bulletin.Fields.startDate), root.get(Bulletin.Fields.endDate));
+        return (root, query, criteriaBuilder) -> criteriaBuilder.between(criteriaBuilder.literal(date), root.get(Bulletin.Fields.startDate), root.get(Bulletin.Fields.endDate));
     }
 }
